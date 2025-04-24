7e1f09bddc70943f23b8f6cf0fd0deeca49aa7e3
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2025-04-24 19:17:43 +0200

    feat: add CEP and TSLA to price ticker

--- src/components/PriceTicker.tsx
+++ src/components/PriceTicker.tsx
@@ -16,7 +16,9 @@ const TICKERS: Price[] = [
   { symbol: 'MSTR', tradingViewSymbol: 'NASDAQ:MSTR', exchange: 'NASDAQ', color: 'text-white' },
   { symbol: 'MSTY', tradingViewSymbol: 'AMEX:MSTY', exchange: 'AMEX', color: 'text-yellow-500' },
   { symbol: 'STRF', tradingViewSymbol: 'NASDAQ:STRF', exchange: 'NASDAQ', color: 'text-white' },
-  { symbol: 'STRK', tradingViewSymbol: 'NASDAQ:STRK', exchange: 'NASDAQ', color: 'text-white' }
+  { symbol: 'STRK', tradingViewSymbol: 'NASDAQ:STRK', exchange: 'NASDAQ', color: 'text-white' },
+  { symbol: 'CEP', tradingViewSymbol: 'NASDAQ:CEP', exchange: 'NASDAQ', color: 'text-white' },
+  { symbol: 'TSLA', tradingViewSymbol: 'NASDAQ:TSLA', exchange: 'NASDAQ', color: 'text-white' },
 ];
 
 export default function PriceTicker() {
