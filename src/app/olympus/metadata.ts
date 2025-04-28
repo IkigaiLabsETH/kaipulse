f38fa27544cbf9621a1988fa632771964ed8d8c9
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2025-04-29 01:20:40 +0200

    feat: update defi page and add olympus metadata

---
+++ src/app/olympus/metadata.ts
@@ -0,0 +1,6 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'OlympusDAO: From Reflexive Bootstrapping to Elastic Hardness | LTL',
+  description: 'Explore how OlympusDAO evolved from a high-APY DeFi experiment to a sophisticated protocol with sustainable monetary policies.',
+} 
