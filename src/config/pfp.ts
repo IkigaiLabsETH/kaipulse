1a84530d59b2615afb4c62db56cdc714ae876b26
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2025-05-12 14:16:56 +0200

    Remove outdated PFP collection slugs from configuration file for improved accuracy.

--- src/config/pfp.ts
+++ src/config/pfp.ts
@@ -9,15 +9,8 @@
 export const PFP_COLLECTION_SLUGS_OR_ADDRESSES = [
   'cryptopunks',
   'official-v1-punks',
-  'grifters-by-xcopy',
   'pudgypenguins',
-  'boredapeyachtclub',
-  'doodles-official',
-  'good-vibes-club',  
-  'degods-eth', 
   'azuki', 
   'rektguy',
-  'meebits',
-  'cryptodickbutts-s3',
-  'mfers'
+  'cryptodickbutts-s3'
 ]; 
